19f1a3879a8a20d3de0bdb0115ad17b559443556
rostov <rostov@simicon.com>
2021-09-07 13:02:26 +0300

    index.

---
+++ index.php
@@ -0,0 +1,18 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
+h1 {
+  background-color: green;
+}
+</style>
+</head>
+<body>
+<?php
+echo "<h1>HELLO ElasticBeansTalk!</h1>";
+?>
+</body>
+</html> 
